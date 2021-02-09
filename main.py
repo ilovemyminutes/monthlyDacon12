b64bc3050baf3105ce02e44c52d8437ac107dae6
iloveslowfood <silkstaff@naver.com>
2021-02-09 08:55:08 +0000

    run black

--- main.py
+++ main.py
@@ -13,44 +13,51 @@ from core.load.load_data import dirtyMNISTDataset
 
 
 def get_config():
-    parser = argparse.ArgumentParser()
+    # parser = argparse.ArgumentParser()
     return
 
+
 def get_data(batch_size: int = 16):
-    print(f'Getting Data...')
-    
+    print(f"Getting Data...")
+
     # transform = transforms.Compose([transforms.Grayscale(), transforms.ToTensor()])
-    train_dataset = dirtyMNISTDataset(mode='train', transform=None)
-    valid_dataset = dirtyMNISTDataset(mode='valid', transform=None)
+    train_dataset = dirtyMNISTDataset(mode="train", transform=None)
+    valid_dataset = dirtyMNISTDataset(mode="valid", transform=None)
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size)
     valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size)
-    
+
     return train_loader, valid_loader
 
-def get_model(lr: float = 0.01, device: str):
+
+def get_model(lr: float, device: str):
     model = VanillaCNN().to(device)
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     return model, loss_function, optimizer
 
-def train_model(model: nn.Module, 
-                loss_function,
-                optimizer, 
-                train_loader: DataLoader, valid_loader: DataLoader, 
-                epochs:int=3, batch_size:int=16, device:str=None):
-    
+
+def train_model(
+    model: nn.Module,
+    loss_function,
+    optimizer,
+    train_loader: DataLoader,
+    valid_loader: DataLoader,
+    epochs: int = 3,
+    batch_size: int = 16,
+    device: str = None,
+):
     model.train()
     print_every = 1
-    
+
     # train phase
     for epoch in range(epochs):
 
         loss_val_sum = 0
 
         for batch in tqdm(train_loader):
-            X = batch['image'].to(device)
-            y_true = batch['label'].to(device)
+            X = batch["image"].to(device)
+            y_true = batch["label"].to(device)
 
             if len(X.size()) == 3:  # channel=1
                 N, H, W = X.size()
@@ -60,20 +67,17 @@ def train_model(model: nn.Module,
 
             y_pred = model.forward(X.view(-1, C, H, W).to(device))
             loss = loss_function(y_pred, y)
-            
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             loss_val_sum += loss
-        
+
         if ((epoch % print_every) == 0) or (epoch == (epochs - 1)):
             loss_val_avg = loss_val_sum / len(train_loader)
             accr_val = evaluate(model=model, data_iter=valid_loader, device=device)
 
-if __name__ == '__main__':
-    config = get_config()
-    
 
-
-            
+# if __name__ == '__main__':
+#     config = get_config()
